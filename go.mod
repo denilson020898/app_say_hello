fd7e06f4f287753d1896e60e63241d85c9353113
Denilson <denilson020898@gmail.com>
2020-12-07 12:53:27 +0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/denilson020898/app_say_hello
+
+go 1.15
+
+require github.com/denilson020898/go_say_hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/denilson020898/go_say_hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
